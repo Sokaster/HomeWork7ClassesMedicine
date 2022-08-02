2fdb2d26622bac3a5b90fe7e85a32e58f461a83e
unknown <danenkov.dmitriy@list.ru>
2022-08-02 23:01:35 +0300

    add

---
+++ DrobiDop/Drobi.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrobiDop
+{
+    internal class Drobi
+    {
+        public int _N;
+        public int _D;
+
+        public Drobi(int numerator, int denominator)
+        {
+            _N = numerator;
+            _D = denominator;
+        }
+
+        public Drobi(int num) : this(num, 1)
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{_N}/{_D}");
+        }
+
+        public static Drobi operator +(Drobi f1, Drobi f2)
+        {
+            return new Drobi(f1._N * f2._D + f2._N * f1._D, f1._D * f2._D).Normalization();
+        }
+
+        public static Drobi operator *(Drobi f1, Drobi f2)
+        {
+            return new Drobi(f1._N * f2._N, f1._D * f2._D).Normalization();
+        }
+
+        public static Drobi operator -(Drobi f1, Drobi f2)
+        {
+            return new Drobi(f1._N * f2._D - f2._N * f1._D, f1._D * f2._D).Normalization();
+        }
+
+        public static Drobi operator -(Drobi f1)
+        {
+            return new Drobi(-f1._N, f1._D).Normalization();
+        }
+
+        public static Drobi operator /(Drobi f1, Drobi f2)
+        {
+            return new Drobi(f1._N * f2._D, f1._D * f2._N).Normalization();
+        }
+
+        public Drobi Normalization()
+        {
+            var n = Math.Abs(_N);
+            var d = Math.Abs(_D);
+            var nod = NOD(n, d);
+            var sign = Math.Sign(_N * _D);
+            return new Drobi(sign * n / nod, d / nod);
+        }
+
+        static int NOD(int a, int b)
+        {
+            while (a > 0 && b > 0)
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+
+            return a + b;
+        }
+    }
+}
---
+++ DrobiDop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DrobiDop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var f1 = new Drobi(4, 11);
+            var f2 = new Drobi(6, 9);
+
+            var f3 = f1 + f2;
+
+            Console.WriteLine($"{f1} + {f2} = {f3}");
+            Console.ReadLine();
+        }
+    }
+}
